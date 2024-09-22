629656efab53dffd63b50bee235f10ff94e4b22a
MathiasGrave <Grave@10.3.60.220>
2024-09-22 12:26:27 +0200

    Finalisation de la partie 1

--- TP0_Exercice1/src/tp0_exercice1/TP0_Exercice1.java
+++ TP0_Exercice1/src/tp0_exercice1/TP0_Exercice1.java
@@ -1,12 +1,13 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+* TP0 Exercice 1
+*Mathias Grave   
+*Septembre 2024
  */
 package tp0_exercice1;
 
 /**
  *
- * @author Mathias Grave
+ * @author Grave
  */
 public class TP0_Exercice1 {
 
@@ -14,7 +15,15 @@ public class TP0_Exercice1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bonjour");
+        System.out.print("Au revoir");
+        int age;
+        Sscanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre âge ?");
+        age=sc.nextLine();
+        
+        
     }
     
 }
