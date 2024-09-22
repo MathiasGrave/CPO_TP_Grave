fa450fd450af021cfaf2c085cd4fccb5fb46f532
MathiasGrave <Grave@10.3.60.220>
2024-09-22 12:36:55 +0200

    Création d'un nouveau projet

---
+++ TP0_Exercie2/src/tp0_exercie2/TP0_Exercie2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_exercie2;
+
+/**
+ *
+ * @author Grave
+ */
+public class TP0_Exercie2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
