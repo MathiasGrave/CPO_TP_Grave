8b24a5845ee24827720a331f30cafb42d8a6cc98
MathiasGrave <Grave@10.3.60.220>
2024-09-22 12:46:31 +0200

    Finalisation de la partie 2

--- TP0_Exercie2/src/tp0_exercie2/TP0_Exercie2.java
+++ TP0_Exercie2/src/tp0_exercie2/TP0_Exercie2.java
@@ -1,8 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP0
+ * Exercice 2
+ * Mathias Grave
  */
 package tp0_exercie2;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +17,25 @@ public class TP0_Exercie2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//Declaration des variables
+int nb;
+
+//nb=5;
+Scanner sc = new Scanner(System.in);
+System.out.println("\n Entrer le nombre :");
+nb=sc.nextInt(); // On demande a sc de donner le prochain entier// nombre dentiers a additionner
+int result; // resultat
+int ind; //indice
+
+result=0;
+// Addition des nb premiers entiers
+ind=1;
+while (result <= nb) {
+result=result+ind;
+}
+// Affichage du resultat
+System.out.println();
+System.out.println("La somme des "+ nb + " entiers est: "+result);
     }
     
 }
