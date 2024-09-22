8e3f529952a24a8d572a0ee6464f9624a6932066
MathiasGrave <Grave@10.3.60.220>
2024-09-22 13:17:54 +0200

    Finalisation de l'exercice 6 de la partie 3 du TP0

--- TP0_Exercice3/src/tp0_exercice3/TP0_Exercice3.java
+++ TP0_Exercice3/src/tp0_exercice3/TP0_Exercice3.java
@@ -1,8 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ *Partie 3 
+ *Mathias Grave
+ *Septembre 2024
  */
 package tp0_exercice3;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +17,49 @@ public class TP0_Exercice3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int operator;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter the operator :");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+        
+        operator=sc.nextInt();
+        int operande1;
+        Scanner sc1=new Scanner(System.in);
+        System.out.println("Entrez la première valeur :");
+        operande1=sc1.nextInt();
+        
+        int operande2;
+        Scanner sc2=new Scanner(System.in);
+        System.out.println("Entrez la deuxième valeur :");
+        operande2=sc2.nextInt();
+        int resultat;
+        if (operator==1) {
+            resultat=operande1+operande2;
+            System.out.println("Le résultat est : "+ resultat);
+        }
+        else if (operator==2) {
+            resultat=operande1-operande2;
+            System.out.println("Le résultat est : "+resultat);
+        }
+        else if (operator==3) {
+            resultat=operande1*operande2;
+            System.out.println("Le résultat est : "+resultat);
+        }
+        else if (operator==4) {
+            resultat=operande1/operande2;
+            System.out.println("Le résultat est : "+resultat);
+        }
+        else if (operator==5) {
+            resultat=operande1%operande2;
+            System.out.println("Le résultat est : "+resultat);
+        }
+        else {
+            System.out.println("L'opérateur choisi n'est pas valide");
+        }
     }
     
 }
