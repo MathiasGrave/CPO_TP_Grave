ac493cd00de55941db6fedc642bf793ecaf32830
MathiasGrave <Grave@10.3.60.220>
2024-09-23 17:44:14 +0200

    Finalisation de l'exercie 1 du TP1

--- TP1_Exercice1_Grave/src/tp1_exercice1_grave/TP1_ManipNombresInt_Grave.java
+++ TP1_Exercice1_Grave/src/tp1_exercice1_grave/TP1_ManipNombresInt_Grave.java
@@ -18,27 +18,31 @@ public class TP1_ManipNombresInt_Grave {
      */
     public static void main(String[] args) {
         int nb1;
+        //On initialise la variable 1
         int nb2;
+        //On initialise la variable 2
         Scanner sc= new Scanner(System.in);
         System.out.println("Entrez un premier nombre :");
         nb1=sc.nextInt();
         Scanner nb=new Scanner(System.in);
         System.out.println("Entrez un deuxieme nombre :");
         nb2=nb.nextInt();
-        System.out.println("Le premier nombre est "+nb1+" et le deuxieme nombre est "+nb2);
+        //On demande à l'utilisateur d'entrer les valeurs de ces variables.
         int somme;
         int produit;
         int difference;
+        //On initialise Les variables qui vont stocker les différentes opérations demandés
         difference=nb1-nb2;
         produit=nb1*nb2;
         somme=nb1+nb2;
         System.out.println("La somme de ces 2 nombre est : "+somme+" ; leur difference est : "+difference+" ; et leur produit est : "+produit);
+        //On affiche les résultats des différentes opérations
         int quotient;
         int reste;
         quotient=nb1/nb2;
         reste=nb1%nb2;
         System.out.println("Leur quotient entier est : "+quotient+" ; et le reste est : "+reste);
-        
+        //On réitère pour le quotient entier et le reste de la division euclidienne
     }
     
 }
