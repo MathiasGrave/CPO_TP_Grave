ade24f8160e2df566dd1e08922124b0bc727f095
MathiasGrave <Grave@10.3.60.220>
2024-10-23 01:10:11 +0200

    Création de l'exercice 3 du TP 2

---
+++ TP2_manip_GRAVE/src/tp2_manip_grave/Moussaka.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_grave;
+
+/**
+ *
+ * @author Grave
+ */
+public class Moussaka {
+    int nbCalories;
+    public Moussaka(int Calories) {
+    Calories=nbCalories;
+}
---
+++ TP2_manip_GRAVE/src/tp2_manip_grave/Tartiflette.java
@@ -0,0 +1,17 @@
+package tp2_manip_grave;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Grave
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette(int Calories) {
+        nbCalories=Calories;
+    }
+}
