6fe0c83b8cdc2bcb529226e626fead74bff405fa
MathiasGrave <Grave@10.3.60.220>
2024-10-23 01:17:33 +0200

    Création de l'exercice 4 du TP 2

---
+++ TP2_relation_GRAVE/src/tp2_relation_grave/Personne.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_grave;
+
+/**
+ *
+ * @author Grave
+ */
+public class Personne {
+    String nom;
+    String prenom;
+    public Personne(String UnNom, String UnPrenom){
+        UnNom=nom;
+        UnPrenom=prenom;  
+    }
+    @Override
+    
+}
---
+++ TP2_relation_GRAVE/src/tp2_relation_grave/Voiture.java
@@ -0,0 +1,14 @@
+package tp2_relation_grave;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Grave
+ */
+public class Voiture {
+    
+}
