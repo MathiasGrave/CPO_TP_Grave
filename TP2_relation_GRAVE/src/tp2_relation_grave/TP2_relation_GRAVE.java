8b76ed1b64c2ee36765e9ca6edcd005dd4e23e51
MathiasGrave <Grave@10.3.60.220>
2024-10-27 12:20:36 +0100

    Suite de l'exercice 4 du TP 2

--- TP2_relation_GRAVE/src/tp2_relation_grave/TP2_relation_GRAVE.java
+++ TP2_relation_GRAVE/src/tp2_relation_grave/TP2_relation_GRAVE.java
@@ -21,13 +21,30 @@ public class TP2_relation_GRAVE {
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
         System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra);
-        bob.liste_voitures[0] = uneClio ;
-        bob.nbVoiture = 1 ;
-        uneClio.proprietaire = bob ;
+        //bob.liste_voitures[0] = uneClio ;
+        //bob.nbVoiture = 1 ;
+        //uneClio.proprietaire = bob ;
+        //System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
+        //bob.liste_voitures[1]=une2008;
+        //bob.nbVoiture=2;
+        //une2008.proprietaire = bob;
+        //System.out.println("la deuxieme voiture de Bob est " +bob.liste_voitures[1] ) ;
+        //reno.liste_voitures[0]= uneAutreClio;
+        //reno.nbVoiture=1;
+        //uneAutreClio.proprietaire=reno;
+        //System.out.println("la premiere voiture de Reno est " +reno.liste_voitures[0] );
+        //uneMicra.proprietaire=reno;
+        //reno.liste_voitures[1]=uneMicra;
+        //reno.nbVoiture=2;
+        //uneMicra.proprietaire=reno;
+        //System.out.println("la deuxieme voiture de Reno est " +reno.liste_voitures[1] );
+        bob.ajouter_une_voiture(uneClio);
         System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
-        bob.liste_voitures[1]=une2008;
-        bob.nbVoiture=2;
-        
-    }
-    
+        bob.ajouter_une_voiture(une2008);
+        System.out.println("la deuxieme voiture de Bob est " +bob.liste_voitures[1] ) ;
+        reno.ajouter_une_voiture(uneAutreClio);
+        System.out.println("la premiere voiture de Reno est " +reno.liste_voitures[0] ) ;
+        reno.ajouter_une_voiture(uneMicra);
+        System.out.println("la deuxieme voiture de Reno est " +reno.liste_voitures[1] ) ;
+    }           
 }
