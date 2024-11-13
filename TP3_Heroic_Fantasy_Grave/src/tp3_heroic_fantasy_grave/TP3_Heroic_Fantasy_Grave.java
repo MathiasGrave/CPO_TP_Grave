19483b1aa57499df6eece190aa5a4e45943b24c7
MathiasGrave <Grave@10.3.60.220>
2024-11-13 18:24:52 +0100

    Suite du TP3 Exercice 1

--- TP3_Heroic_Fantasy_Grave/src/tp3_heroic_fantasy_grave/TP3_Heroic_Fantasy_Grave.java
+++ TP3_Heroic_Fantasy_Grave/src/tp3_heroic_fantasy_grave/TP3_Heroic_Fantasy_Grave.java
@@ -4,6 +4,8 @@
  */
 package tp3_heroic_fantasy_grave;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Grave
@@ -14,6 +16,19 @@ public class TP3_Heroic_Fantasy_Grave {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Epee Excalibur = new Epee("Excalibur", 7, 5);
+        Epee Durandal = new Epee("Durandal", 4, 7);
+        Baton Chene = new Baton ("Chêne", 4, 5);
+        Baton Charme = new Baton("Chrame", 5, 6);
+        ArrayList<Arme> armes = new ArrayList<Arme>();
+        armes.add(Chene);
+        armes.add(Charme);
+        armes.add(Excalibur);
+        armes.add(Durandal);
+        int longueur=armes.size();
+        for (int i=0; i<longueur; i++) {
+            System.out.println("L'arme n°"+i+"est : "+armes.get(i));
+        }
         
     }
     
